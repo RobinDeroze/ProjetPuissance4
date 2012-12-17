1b0e3b544c1c757b7b041600fa4c3c2b2e209e71
IMERIR <imerir@LIFEBOOK-A530-2.(none)>
2012-12-17 15:52:02 +0100

    Main.java

---
+++ main.java
@@ -0,0 +1,20 @@
+import javax.swing.JFrame;
+
+
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		IHM Mon_Ihm = new IHM();
+		Mon_Ihm.pack();
+		Mon_Ihm.setVisible(true);
+		Mon_Ihm.setSize(700, 723);
+		Mon_Ihm.setResizable(false);
+		Mon_Ihm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+
+}
